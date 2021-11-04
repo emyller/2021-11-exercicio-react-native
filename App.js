afa1a7cecae19bf12d7362ef39b94b7286439439
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-03 23:25:42 -0300

    Implementa tela de lista de filmes

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import axios from "axios";
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from "react-native-gesture-handler";
+
+const Stack = createStackNavigator();
+
+function ListaFilmes(props) {
+  var [filmes, setFilmes] = useState(null);
+  var [requisicaoConcluida, setRequisicaoConcluida] = useState(false);
+
+  if (! requisicaoConcluida) {
+    axios.get('https://sujeitoprogramador.com/r-api/?api=filmes').then(function (resposta) {
+      console.log(resposta.data);
+      setFilmes(resposta.data);
+      setRequisicaoConcluida(true);
+    });
+  }
+
+  return (
+    <View>
+      <Text>Lista de filmes</Text>
+      {filmes && filmes.map(function (filme, i) {
+        return (
+          <View key={i}>
+            <Text>Filme: {filme.nome}</Text>
+            <Image source={{ uri: filme.foto }} style={{width: 100, height: 100}} />
+            <Button title="Ver detalhes" onPress={function () {
+              props.navigation.navigate('Detalhes do filme', {
+                sinopse: filme.sinopse,
+              });
+            }} />
+          </View>
+        );
+      })}
+    </View>
+  )
+}
+
+function DetalhesFilme(props) {
+  console.log(props);
+  return (
+    <View>
+      <Text style={{ fontWeight: 'bold' }}>Sinopse:</Text>
+      <Text>{props.route.params.sinopse}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Lista de filmes" component={ListaFilmes} />
+        <Stack.Screen name="Detalhes do filme" component={DetalhesFilme} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
